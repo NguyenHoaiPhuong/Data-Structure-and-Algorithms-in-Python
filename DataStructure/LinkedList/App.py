a2808ad874e53aa4eb2aaafd13682cfaa775b420
NguyenHoaiPhuong <hoaiphuong.nguyen.vn@gmail.com>
2018-02-14 02:28:46 +0900

    LinkedList: Each Element in the list is unique. Add 3 important functions for the LinkedList: InsertFirst (O(1)), InsertLast (O(n)) and Remove (O(n)).

---
+++ DataStructure/LinkedList/App.py
@@ -0,0 +1,13 @@
+from DataStructure.LinkedList.LinkedList import CLinkedList
+from DataStructure.LinkedList.LinkedList import CNode
+
+Akagi = CLinkedList()
+Akagi.InsertLast(CNode(1))
+Akagi.InsertLast(CNode(2))
+Akagi.InsertLast(CNode(3))
+Akagi.InsertLast(CNode(4))
+Akagi.InsertLast(CNode(5))
+Akagi.InsertLast(CNode(6))
+Akagi.InsertLast(CNode(7))
+Akagi.Remove(5)
+print(Akagi)
---
+++ DataStructure/LinkedList/Node.py
@@ -0,0 +1,20 @@
+class CNode(object):
+    def __init__(self, value = None, next = None):
+        self.value = value
+        self.next = next
+
+    def __str__(self):
+        Res = "Value = " + str(self.value) + ", Next Node is " + str(self.next)
+        return Res
+
+    def SetValue(self, value):
+        self.value = value
+
+    def GetValue(self):
+        return self.value
+
+    def SetNextNode(self, next):
+        self.next = next
+
+    def GetNextNode(self):
+        return self.next
